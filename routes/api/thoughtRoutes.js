15d0f3b6e6b03a2ad7dff5f699cecd11359c2774
Sam Brez <sambreslin02@gmail.com>
2024-02-17 12:54:40 -0500

    updated routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,24 +11,24 @@ const {
 } = require('../../controllers/thoughtController');
 
 // GET all thoughts
-router.route('/').get();
+router.route('/').get(getThoughts);
 
 // GET a single thought by its _id 
-router.route('/:thoughtId').get();
+router.route('/:thoughtId').get(getSingleThought);
 
 // POST a new thought
-router.route('/').get().post();
+router.route('/').get(getThoughts).post(createThought);
 
 // PUT to update a thought by its _id
-router.route('/:thoughtId').get().put();
+router.route('/:thoughtId').get(getSingleThought).put(updateThought);
 
 // DELETE to remove a thought by its _id
-router.route('/:thoughtId').delete();
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
 
 // POST to create a reaction stored in a single thought's reactions array field
-router.route('/:thoughtId/reactions').get().post();
+router.route('/:thoughtId/reactions').get(getSingleThought).post(createReaction);
 
 // DELETE to pull and remove a reaction by the reaction's reactionId value
-router.route('/:thoughtId/reactions/:reactionId').delete();
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -11,24 +11,24 @@ const {
 } = require('../../controllers/userController');
 
 // GET all users
-router.route('/').get();
+router.route('/').get(getUsers);
 
 // GET a single user by its _id and populated thought and friend data
-router.route('/:userId').get();
+router.route('/:userId').get(getSingleUser);
 
 // POST a new user:
-router.route('/').get().post();
+router.route('/').get(getUsers).post(createUser);
 
 // PUT to update a user by its _id
-router.route('/:userId').get().put();
+router.route('/:userId').get(getSingleUser).put(updateUser);
 
 // DELETE to remove user by its _id
-router.route('/:userId').delete();
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
 
 // POST to add a new friend to a user's friend list
-router.route('/:userId/friends/:friendId').get().post();
+router.route('/:userId/friends/:friendId').get(getSingleUser).post(addFriend);
 
 // DELETE to remove a friend from a user's friend list
-router.route('/:userId/friends/:friendId').delete();
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
 
 module.exports = router;
